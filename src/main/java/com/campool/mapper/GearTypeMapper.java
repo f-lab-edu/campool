f181a586a1fd3efb3405d00d99c0c7e7525eb974
Habist <liasn@naver.com>
2021-03-21 20:04:25 +0900

    refactor: [#26] selectGearTypes 주석문 추가
    해당 쿼리에 대한 설명을 나타내는 주석문을 추가하였습니다.

--- src/main/java/com/campool/mapper/GearTypeMapper.java
+++ src/main/java/com/campool/mapper/GearTypeMapper.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface GearTypeMapper {
 
+    /**
+     * 렌트정보 조회 시 참조하는 캠핑용품 전체 유형을 가져오는 쿼리
+     */
     @Select("SELECT ID, NAME FROM TYPE")
     List<GearType> selectGearTypes();
 
