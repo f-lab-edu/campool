fb8a2ee5a763394deefe81559829bc8ed94b5f44
Habist <liasn@naver.com>
2021-03-22 21:08:36 +0900

    refactor: [#26] 캠핑용품 유형 데이터 조회 쿼리 주석문 수정

--- src/main/java/com/campool/mapper/GearTypeMapper.java
+++ src/main/java/com/campool/mapper/GearTypeMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
 public interface GearTypeMapper {
 
     /**
-     * 캠핑용품 유형 데이터는 많지 않기 때문에 테이블 전체를 불러와 캐싱하기 위한 용도
+     * 캠핑용품 유형 데이터는 많지 않기 때문에 테이블 전체를 불러와 캐싱하기 위한 용도의 쿼리
      */
     @Select("SELECT ID, NAME FROM TYPE")
     List<GearType> selectGearTypes();
