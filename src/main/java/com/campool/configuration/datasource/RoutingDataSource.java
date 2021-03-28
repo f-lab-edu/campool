697ecb303e1866e7dae281f3f65e89098d601e6a
Habist <liasn@naver.com>
2021-03-28 23:04:26 +0900

    refactor: [#19] DataSource Master/Slave 문자열 상수로 변경

--- src/main/java/com/campool/configuration/datasource/RoutingDataSource.java
+++ src/main/java/com/campool/configuration/datasource/RoutingDataSource.java
@@ -1,5 +1,8 @@
 package com.campool.configuration.datasource;
 
+import static com.campool.configuration.DataSourceConfiguration.MASTER;
+import static com.campool.configuration.DataSourceConfiguration.SLAVE;
+
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -8,6 +11,6 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         return TransactionSynchronizationManager.isCurrentTransactionReadOnly() ?
-                "slave" : "master";
+                SLAVE : MASTER;
     }
 }
