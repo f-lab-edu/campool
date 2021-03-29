ca2f05f263085f5579886a8deaa36633362715c7
Habist <liasn@naver.com>
2021-03-30 01:21:16 +0900

    feat: [#36] 캠핑용품 타입 데이터 캐시 적용
    Spring의 CuncurrentHashMap 기반 캐시를 캠핑용품 타입 데이터 조회 기능에 적용했습니다.

--- src/main/java/com/campool/CampoolApplication.java
+++ src/main/java/com/campool/CampoolApplication.java
@@ -2,8 +2,10 @@ package com.campool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableCaching
 @EnableRedisHttpSession
 @SpringBootApplication
 public class CampoolApplication {
