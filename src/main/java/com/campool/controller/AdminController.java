5406199c0acb401cc9b4e92a94bf68baeb424f37
Habist <liasn@naver.com>
2021-04-09 01:29:03 +0900

    feat: [#39] 관리자 로그아웃 기능 추가
    관리자 로그아웃 기능을 추가하였습니다.
    관리자에 맞게 메소드 명을 변경하였습니다.

--- src/main/java/com/campool/controller/AdminController.java
+++ src/main/java/com/campool/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.campool.controller;
 
+import com.campool.annotation.LoginValidation;
 import com.campool.model.AdminLoginRequest;
 import com.campool.model.AdminSignUp;
 import com.campool.service.AdminService;
@@ -7,6 +8,7 @@ import com.campool.service.AuthService;
 import javax.validation.Valid;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +23,19 @@ public class AdminController {
     private final AuthService authService;
 
     @PostMapping("/admins")
-    public void signUpUser(@Valid AdminSignUp adminSignUp) {
+    public void signUpAdmin(@Valid AdminSignUp adminSignUp) {
         adminService.add(adminSignUp);
     }
 
     @PostMapping("/admins/login")
-    public void loginUser(@Valid AdminLoginRequest adminLoginRequest) {
+    public void loginAdmin(@Valid AdminLoginRequest adminLoginRequest) {
         authService.authenticate(adminLoginRequest);
+    }
+
+    @LoginValidation
+    @GetMapping("/users/logout")
+    public void logoutAdmin() {
+        authService.deauthenticate();
     }
 
 }
