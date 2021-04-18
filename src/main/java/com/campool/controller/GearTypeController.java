4184e0a043c08483cad5f56ad802bbd2673d48f1
Habist <liasn@naver.com>
2021-04-18 23:09:36 +0900

    feat: [#35] 캠핑용품 유형 데이터 등록 기능 추가
    캠핑용품 유형 데이터 등록 요청을 위한 컨트롤러 메소드를 추가하였습니다.
    캠핑용품 유형 데이터 틍록을 위한 Service를 추가하였습니다.
    캠핑용품 유형 데이터 등록 요청 모델 클래스를 추가하였습니다.

--- src/main/java/com/campool/controller/GearTypeController.java
+++ src/main/java/com/campool/controller/GearTypeController.java
@@ -1,12 +1,16 @@
 package com.campool.controller;
 
 import com.campool.annotation.LoginValidation;
+import com.campool.enumeration.Role;
 import com.campool.model.GearType;
+import com.campool.model.GearTypeRegisterRequest;
 import com.campool.service.GearTypeService;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,6 +24,12 @@ public class GearTypeController {
     @GetMapping("/types")
     public List<GearType> getGearTypes() {
         return gearTypeService.getGearTypes();
+    }
+
+    @LoginValidation(role = Role.ADMIN)
+    @PostMapping("/types")
+    public void registerGearType(@Valid GearTypeRegisterRequest gearTypeRegisterRequest) {
+        gearTypeService.addGearType(gearTypeRegisterRequest.getName());
     }
 
 }
--- src/main/java/com/campool/service/GearTypeService.java
+++ src/main/java/com/campool/service/GearTypeService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,6 +19,17 @@ public class GearTypeService {
     @Cacheable(value = "gearTypes")
     public List<GearType> getGearTypes() {
         return gearTypeMapper.selectGearTypes();
+    }
+
+    public void addGearType(String name) {
+        if (isDuplicateByName(name)) {
+            throw new DuplicateKeyException("이미 등록되어 있는 타입입니다.");
+        }
+        gearTypeMapper.insertGearType(name);
+    }
+
+    public boolean isDuplicateByName(String name) {
+        return gearTypeMapper.findGearTypeByName(name) != null;
     }
 
 }
