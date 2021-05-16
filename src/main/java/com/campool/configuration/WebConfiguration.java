acb58767db974424bef6661800249dd410b8e87a
Habist <liasn@naver.com>
2021-05-17 00:00:34 +0900

    refactor: [#54] LocalDate 필드 Format 글로벌 설정 추가

--- src/main/java/com/campool/configuration/WebConfiguration.java
+++ src/main/java/com/campool/configuration/WebConfiguration.java
@@ -1,8 +1,10 @@
 package com.campool.configuration;
 
+import com.campool.configuration.formatter.LocalDateFormatter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +17,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolverList) {
         argumentResolverList.add(loginUserIdResolver);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
     }
 
 }
