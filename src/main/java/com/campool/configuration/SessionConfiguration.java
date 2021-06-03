6b94318d79edfc9b56fdb623ce2629e0a8dfa00e
Habist <liasn@naver.com>
2021-06-03 21:39:08 +0900

    improve: [#76] Redis Session Config Repository Bean 추가
    병목을 일으키는 RedisIndexedSessionRepository를 사용하지 않도록 EnableSpringHttpSession를 선언하고
    직접 구현한 Repository를 Bean으로 등록

--- src/main/java/com/campool/configuration/SessionConfiguration.java
+++ src/main/java/com/campool/configuration/SessionConfiguration.java
@@ -1,14 +1,23 @@
 package com.campool.configuration;
 
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.RedisSessionRepository;
 
+@EnableSpringHttpSession
 @Configuration
 public class SessionConfiguration {
+
+    public static final long MAX_INACTIVE_INTERVAL = 300;
 
     @Value("${campool.redis.session.host}")
     private String sessionHost;
@@ -22,6 +31,25 @@ public class SessionConfiguration {
         redisStandaloneConfiguration.setHostName(sessionHost);
         redisStandaloneConfiguration.setPort(sessionPort);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisOperations<String, Object> sessionRedisOperations(
+            RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisSessionRepository sessionRepository(
+            RedisOperations<String, Object> sessionRedisOperations) {
+        RedisSessionRepository redisSessionRepository = new RedisSessionRepository(
+                sessionRedisOperations);
+        redisSessionRepository.setDefaultMaxInactiveInterval(Duration.ofSeconds(MAX_INACTIVE_INTERVAL));
+        return redisSessionRepository;
     }
 
 }
