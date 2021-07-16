28612746269cfb72e93e4ab88e294b2c99666e8e
Habist <liasn@naver.com>
2021-07-16 19:24:04 +0900

    refactor: [#78] 멀티라인을 이용해 쿼리 가독성 증가

--- src/main/java/com/campool/mapper/UserMapper.kt
+++ src/main/java/com/campool/mapper/UserMapper.kt
@@ -6,16 +6,39 @@ import org.apache.ibatis.annotations.*
 
 @Mapper
 interface UserMapper {
-    @Insert("INSERT INTO USER(id, password, name, email, telephone) VALUES(#{id}, #{password}, #{name}, #{email}, #{telephone})")
+    @Insert(
+        """
+        INSERT INTO USER(id, password, name, email, telephone) 
+        VALUES(#{id}, #{password}, #{name}, #{email}, #{telephone})
+        """
+    )
     fun insertUser(userSignUpRequest: UserSignUpRequest)
 
-    @Select("SELECT id, name, email, telephone FROM USER WHERE id = #{id}")
+    @Select(
+        """
+        SELECT id, name, email, telephone 
+        FROM USER 
+        WHERE id = #{id}
+        """
+    )
     fun findById(id: String): UserInfo?
 
-    @Select("SELECT id, name, email, telephone FROM USER WHERE id = #{id} AND password = #{password}")
+    @Select(
+        """
+        SELECT id, name, email, telephone 
+        FROM USER 
+        WHERE id = #{id} AND password = #{password}
+        """
+    )
     fun findByIdAndPassword(id: String, password: String): UserInfo?
 
-    @Update("UPDATE USER SET password = #{password}, name = #{name}, email = #{email}, telephone = #{telephone} WHERE id = #{id} ")
+    @Update(
+        """
+        UPDATE USER 
+        SET password = #{password}, name = #{name}, email = #{email}, telephone = #{telephone} 
+        WHERE id = #{id}
+        """
+    )
     fun updateById(id: String, password: String, name: String, email: String, telephone: String)
 
     @Delete("DELETE FROM USER WHERE id = #{id}")
