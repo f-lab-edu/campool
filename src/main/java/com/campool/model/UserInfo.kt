30bb030c23654213bab4f0ef6a01b2c00494d8e6
Habist <liasn@naver.com>
2021-07-20 17:45:55 +0900

    refactor: [#78] 사용자 정보 모델 클래스 nullable 특성 제거

--- src/main/java/com/campool/model/UserInfo.kt
+++ src/main/java/com/campool/model/UserInfo.kt
@@ -1,8 +1,8 @@
 package com.campool.model
 
 data class UserInfo(
-    val id: String? = null,
-    val name: String? = null,
-    val email: String? = null,
-    val telephone: String? = null
+    val id: String,
+    val name: String,
+    val email: String,
+    val telephone: String
 )
